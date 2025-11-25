cca5826d9d7d27e7efcefdb618f590a6237b9f22
Tachera Sasi <tacherasasi@gmail.com>
2025-11-25 11:11:00 +0300

    refactor: remove unnecessary whitespace and reorder import statement

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,3 @@
-const std = @import("std");
-
 pub fn main() !void {
     var stdout_buffer: [256]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
@@ -22,20 +20,23 @@ pub fn main() !void {
     if (@import("builtin").os.tag == .windows) {
         const windows = std.os.windows;
         const kernel32 = windows.kernel32;
-    
+
         const stdout_handle = try std.os.windows.GetStdHandle(std.os.windows.STD_OUTPUT_HANDLE);
-    
+
         var csbi: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
         if (kernel32.GetConsoleScreenBufferInfo(stdout_handle, &csbi) == 0) {
             return error.GetConsoleInfoFailed;
         }
-    
+
         const console_size = @as(u32, @intCast(csbi.dwSize.X)) * @as(u32, @intCast(csbi.dwSize.Y));
         const coord = windows.COORD{ .X = 0, .Y = 0 };
         var written: u32 = 0;
-    
+
         _ = kernel32.FillConsoleOutputCharacterA(stdout_handle, ' ', console_size, coord, &written);
         _ = kernel32.FillConsoleOutputAttribute(stdout_handle, csbi.wAttributes, console_size, coord, &written);
         _ = kernel32.SetConsoleCursorPosition(stdout_handle, coord);
     }
 }
+
+
+const std = @import("std");
