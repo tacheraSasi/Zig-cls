d953b3e11bf480006da337908274af14d30595f1
Tachera Sasi <tacherasasi@gmail.com>
2025-11-25 11:09:53 +0300

    refactor: update output message and ensure proper formatting for screen clearing

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ pub fn main() !void {
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
 
-    try stdout.print("greetings, program, welcome to the {s}, a {s}\n", .{ "grid", "digital frontier" });
+    try stdout.print("clearing...\n", .{});
     try stdout.flush();
 
     // Use ANSI escape codes which work on:
@@ -15,27 +15,27 @@ pub fn main() !void {
     // - Windows Terminal
 
     // Clear screen and move cursor to top-left
-    try stdout.print("\x1B[2J\x1B[H");
+    try stdout.print("\x1B[2J\x1B[H", .{});
     try stdout.flush();
 
     // Alternative method for older Windows systems:
-    // if (@import("builtin").os.tag == .windows) {
-    //     const windows = std.os.windows;
-    //     const kernel32 = windows.kernel32;
-    //
-    //     const stdout_handle = try std.os.windows.GetStdHandle(std.os.windows.STD_OUTPUT_HANDLE);
-    //
-    //     var csbi: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
-    //     if (kernel32.GetConsoleScreenBufferInfo(stdout_handle, &csbi) == 0) {
-    //         return error.GetConsoleInfoFailed;
-    //     }
-    //
-    //     const console_size = @as(u32, @intCast(csbi.dwSize.X)) * @as(u32, @intCast(csbi.dwSize.Y));
-    //     const coord = windows.COORD{ .X = 0, .Y = 0 };
-    //     var written: u32 = 0;
-    //
-    //     _ = kernel32.FillConsoleOutputCharacterA(stdout_handle, ' ', console_size, coord, &written);
-    //     _ = kernel32.FillConsoleOutputAttribute(stdout_handle, csbi.wAttributes, console_size, coord, &written);
-    //     _ = kernel32.SetConsoleCursorPosition(stdout_handle, coord);
-    // }
+    if (@import("builtin").os.tag == .windows) {
+        const windows = std.os.windows;
+        const kernel32 = windows.kernel32;
+    
+        const stdout_handle = try std.os.windows.GetStdHandle(std.os.windows.STD_OUTPUT_HANDLE);
+    
+        var csbi: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
+        if (kernel32.GetConsoleScreenBufferInfo(stdout_handle, &csbi) == 0) {
+            return error.GetConsoleInfoFailed;
+        }
+    
+        const console_size = @as(u32, @intCast(csbi.dwSize.X)) * @as(u32, @intCast(csbi.dwSize.Y));
+        const coord = windows.COORD{ .X = 0, .Y = 0 };
+        var written: u32 = 0;
+    
+        _ = kernel32.FillConsoleOutputCharacterA(stdout_handle, ' ', console_size, coord, &written);
+        _ = kernel32.FillConsoleOutputAttribute(stdout_handle, csbi.wAttributes, console_size, coord, &written);
+        _ = kernel32.SetConsoleCursorPosition(stdout_handle, coord);
+    }
 }
